8531db01e7eb2db6c4b7dcbe0eb8c30ad0c5ce2c
jwilliamson24 <jasmine.m.williamson@gmail.com>
2024-01-29 10:42:49 -0800

    start analysis

---
+++ code/data_analysis.R
@@ -0,0 +1,11 @@
+# Do some analysis with the data I just generated in the other file
+
+# clear workspace, remove graphics code blah blah
+
+# Load data
+filename <- "data/popdata.Rdata"
+load(filename)
+rm(filename)
+
+# Do density dependent analysis
+# Add some description of density dependent hypothesis/ecology
